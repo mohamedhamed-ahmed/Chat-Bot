dc301aff5473c6497644fb1d6796c88c89965e1f
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2025-12-25 15:27:12 +0100

    set up prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,20 @@
-import express from "express";
-import dotenv from "dotenv";
-import type { Request, Response } from "express";
+import express from 'express';
+import dotenv from 'dotenv';
+import type { Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 dotenv.config();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(`Hello World!`);
+app.get('/', (req: Request, res: Response) => {
+   res.send(`Hello World!`);
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({message: "Hello World!"});
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello World!' });
 });
 
 app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server is running on http://localhost:${port}`);
 });
