b4575f40681a58266dbc348e1a5058eb7391e37a
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2025-12-25 14:20:47 +0100

    adding openai key

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 import type { Request, Response } from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+dotenv.config();
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!!");
+  res.send(`${process.env.OPENAI_API_KEY}`);
 });
 
 app.listen(3000, () => {
