9090b3f439fad9eedd44ad07d35ccc0b714a40c7
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2025-12-26 16:51:43 +0100

    refactoring to routes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,24 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import type { Request, Response } from 'express';
-import { chatController } from './controllers/chat.controller';
+import router from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(router);
 
 const port = process.env.PORT || 3000;
 
 dotenv.config();
-
-app.get('/', (req: Request, res: Response) => {
-   res.send(`Hello World!`);
-});
-
-app.post('/api/chat', chatController.sendMessage);
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World!' });
-});
 
 app.listen(3000, () => {
    console.log(`Server is running on http://localhost:${port}`);
