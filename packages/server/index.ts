b7c57b6c4c4f8329752a015d76fe27aa77b86e59
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2025-12-26 16:22:17 +0100

    adding zod for validation

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,59 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import type { Request, Response } from 'express';
+import openai from 'openai';
+import z from 'zod';
 
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 dotenv.config();
 
+const openaiClient = new openai({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
+const conversations = new Map<string, string>();
+
+const conversationSchema = z.object({
+   conversationId: z.uuid(),
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required')
+      .max(1000, 'Prompt is too long (max 1000 characters)'),
+});
+
 app.get('/', (req: Request, res: Response) => {
    res.send(`Hello World!`);
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const parsedResult = conversationSchema.safeParse(req.body);
+   if (!parsedResult.success) {
+      res.status(400).json(parsedResult.error.format());
+      return;
+   }
+
+   try {
+      const { prompt, conversationId } = req.body;
+
+      const response = await openaiClient.responses.create({
+         model: 'gpt-4o-mini',
+         temperature: 0.2,
+         max_output_tokens: 1000,
+         input: prompt,
+         previous_response_id: conversations.get(conversationId),
+      });
+
+      conversations.set(conversationId, response.id);
+
+      res.json({ message: response.output_text });
+   } catch (error) {
+      res.status(500).json({ error: 'Failed to generate response' });
+   }
 });
 
 app.get('/api/hello', (req: Request, res: Response) => {
