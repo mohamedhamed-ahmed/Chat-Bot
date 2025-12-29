603b7ec6f4e8dbb179786cde83f3200455115fad
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2025-12-29 20:06:39 +0100

    added message sounds

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -4,10 +4,17 @@ import TypingIndicator from './TypingIndicator';
 import type { Message } from './ChatMessages';
 import ChatMessages from './ChatMessages';
 import ChatInput, { type ChatFormData } from './ChatInput';
+import notification from '../assets/sounds/notification.mp3';
+import pop from '../assets/sounds/pop.mp3';
 
 type ResponseData = {
    message: string;
 };
+
+const notificationSound = new Audio(notification);
+notificationSound.volume = 0.2;
+const popSound = new Audio(pop);
+popSound.volume = 0.2;
 
 const ChatBot = () => {
    const conversationId = useRef(crypto.randomUUID());
@@ -19,6 +26,7 @@ const ChatBot = () => {
       try {
          setIsTyping(true);
          setErrors('');
+         popSound.play();
          setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
          const { data } = await axios.post<ResponseData>('/api/chat', {
             conversationId: conversationId.current,
@@ -29,6 +37,7 @@ const ChatBot = () => {
             ...prev,
             { content: data.message, role: 'bot' },
          ]);
+         notificationSound.play();
       } catch {
          setErrors('Failed to generate response');
       } finally {
